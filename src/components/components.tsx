489b40ac2be1068f7b9ec4434ea846bca5487d5c
Anthony Menard-Gill <anthonymenardgill@gmail.com>
2024-10-01 00:42:21 -0400

    added validation of url and slug

--- src/components/components.tsx
+++ src/components/components.tsx
@@ -6,14 +6,14 @@ import { FormError } from "types/form";
 export const Renderer = jsxRenderer(({ children }) => {
   return html`
     <!DOCTYPE html>
-    <html>
+    <html data-theme="dark">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://unpkg.com/htmx.org@1.9.3"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.colors.min.css" />
-        <link rel="stylesheet" href="/styles.css" />
-        <title>ANTSH - URLs but smaller</title>
+        <link rel="stylesheet" href="/styles/styles.css" />
+        <title>ANTSH - URLs but shorter</title>
       </head>
       <body class="flex">
         <h2>ANTSH -- Shorter URLs</h2>
@@ -25,29 +25,23 @@ export const Renderer = jsxRenderer(({ children }) => {
 
 export const Form = ({ short_url, errors }: { short_url: ShortUrl; errors: FormError }) => (
   <form hx-post="/url" hx-swap="outerHTML" id="form">
-    <div>
-      <input
-        autocomplete="off"
-        aria-invalid={errors.url ? true : ""}
-        placeholder="your long url"
-        type="text"
-        name="url"
-        value={short_url.url}
-      />
-    </div>
-    <div hx-target="form" hx-swap="outerHTML">
-      <div class="group" role="group">
-        <input
-          autocomplete="off"
-          aria-invalid={errors.slug ? true : ""}
-          hx-post="/url/validate"
-          type="text"
-          name="slug"
-          placeholder="your cool /slug (Ex: cool --> antsh.one/cool)"
-          value={short_url.slug}
-        />
-      </div>
-    </div>
+    <input
+      autocomplete="off"
+      aria-invalid={errors.url ? true : ""}
+      placeholder="your long url"
+      type="text"
+      name="url"
+      value={short_url.url}
+    />
+    <input
+      autocomplete="off"
+      aria-invalid={errors.slug ? true : ""}
+      type="text"
+      name="slug"
+      placeholder="your cool /slug (Ex: cool --> antsh.one/cool)"
+      value={short_url.slug}
+    />
+
     <div className={errors.url ? "pico-color-red-500" : ""}>{errors.url}</div>
     <div className={errors.slug ? "pico-color-red-500" : ""}>{errors.slug}</div>
     <input type="submit" value="Submit" />
@@ -57,6 +51,8 @@ export const Form = ({ short_url, errors }: { short_url: ShortUrl; errors: FormE
 export const Confirmed = ({ short_url }: { short_url: ShortUrl }) => (
   <section class="flex">
     Success! Your short url is:
-    <button class="outline secondary">{short_url.url}</button>
+    <button class="outline secondary">antsh.one/{short_url.slug}</button>
   </section>
 );
+
+export const Error = () => <div>error</div>;
--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,33 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { requestId } from "hono/request-id";
 import { serveStatic } from "hono/bun";
-import { Renderer, Form, Confirmed } from "./components/components";
+import { Renderer, Form, Confirmed, Error } from "./components/components";
 import { customAlphabet } from "nanoid";
 import { createURL, getURL } from "@/db/url";
 import { ShortUrl } from "@/types/URL";
 import { FormError } from "@/types/form";
+import { z, ZodError } from "zod";
+import { zValidator } from "@hono/zod-validator";
 const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 6);
 
 const app = new Hono();
-
-app.use(logger());
+export const customLogger = (message: string, ...rest: string[]) => {
+  console.log(message, ...rest);
+};
+app.use(logger(customLogger));
 app.use("*", requestId({ limitLength: 1 }));
 app.use("*", serveStatic({ root: "./static" }));
 app.get("*", Renderer);
+
+const schema = z.object({
+  url: z
+    .string({ message: "Please provide a URL" })
+    .url({ message: "Invalid URL format. Please include http:// or https://." }),
+  slug: z
+    .string()
+    .regex(/^[a-zA-Z0-9]+$/, { message: "Invalid slug. Only alphanumeric (a-z, 0-9 is accepted)" })
+    .or(z.literal("")),
+});
 
 app.get("/", (c) => {
   return c.render(
@@ -33,25 +47,35 @@ app.get("/:slug", (c) => {
 
 app.post("/url", async (c) => {
   const body = await c.req.parseBody();
-
   const url = typeof body["url"] === "string" ? body["url"] : "";
   let slug = typeof body["slug"] === "string" ? body["slug"] : "";
-  const short_url: ShortUrl = { url, slug };
+  slug = slug.toLowerCase();
+  let short_url: ShortUrl = { url, slug };
   let errors: FormError = {};
-  if (!url) {
-    errors.url = "Please provide a url";
+
+  try {
+    schema.parse(short_url);
+  } catch (error) {
+    if (!(error instanceof ZodError)) return c.html(<Error short_url={short_url}></Error>);
+
+    let issues = error.errors;
+    console.log(issues);
+    errors.url = issues.filter((issue) => issue.path[0] == "url").map((issue) => issue.message)[0];
+    errors.slug = issues.filter((issue) => issue.path[0] == "slug").map((issue) => issue.message)[0];
     return c.html(<Form short_url={short_url} errors={errors} />);
   }
+
   if (!slug) {
     slug = nanoid();
+    short_url.slug = slug;
   }
   try {
-    slug = slug.toLowerCase();
     createURL(url, slug);
   } catch (error) {
     errors = { slug: "slug in use, pick another" };
     return c.html(<Form short_url={short_url} errors={errors} />);
   }
+  customLogger("Short URL saved:", `URL: ${short_url.url},`, `SLUG: ${short_url.slug}`);
   return c.html(<Confirmed short_url={short_url}></Confirmed>);
 });
 
